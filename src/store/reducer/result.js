d3bfd4698d47a3a36e2f486b8707be529f5630be
billdelvin <billdelvin6@gmail.com>
2020-05-07 22:32:26 +0700

    adding result reducer

---
+++ src/store/reducer/result.js
@@ -0,0 +1,32 @@
+import * as actionTypes from "../actions"
+
+const insitialState = {
+  result: [],
+}
+
+const resultReducer = (state = insitialState, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        result: state.result.concat({ id: new Date(), payload }), // dont use push(), why ? push() can manipulate the original state
+      }
+    case actionTypes.DELETE_RESULT:
+      // not using this way
+      // const id = 2
+      // const newArray = [...state.result]
+      // state.result.splice(id, 1)
+      // console.log(payload) => payload yang disini adalah payload yang isi nya id yang dikirim dari counter.js
+      const updateNewArray = state.result.filter((result) => result.id !== payload)
+      return {
+        ...state,
+        result: updateNewArray,
+      }
+    default:
+      return state
+  }
+}
+
+export default resultReducer
