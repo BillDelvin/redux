ef079f101924c688c89e86135c0da89f1841fd08
billdelvin <billdelvin6@gmail.com>
2020-05-07 22:32:38 +0700

    adding counter reducer

---
+++ src/store/reducer/counter.js
@@ -0,0 +1,45 @@
+import * as actionTypes from "../actions"
+
+const insitialState = {
+  counter: 0,
+}
+
+const counterReducer = (state = insitialState, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case actionTypes.INCREMENT:
+      // ini cara yang sama dalam melakukan manipulasi initialState
+      // tapi kita tidak akan menggunakan cara ini dan bukan ini yang mau di lakukan
+      // const newState = Object.assign({}, state);
+      // newState.counter = state.counter + 1;
+      // return newState;
+      return {
+        ...state,
+        counter: state.counter + 1,
+      }
+    case actionTypes.DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      }
+    case actionTypes.ADD:
+      return {
+        // actio.payload => nama payload mesti sama di dengan yang di counter.js di mapDispatchToProps
+        ...state,
+        counter: state.counter + payload, //payload disini dari file Counter.js
+      }
+    case actionTypes.SUBSTRACT:
+      return {
+        ...state,
+        counter: state.counter - payload, //payload disini dari file Counter.js
+      }
+    default:
+      return state
+  }
+}
+
+export default counterReducer
+
+
+// axios.post("url", payload)
